54bd4fbbd87924141de72ab67ad9ed816afa2047
Rishi <rishibhargawa47@gmail.com>
2026-03-01 09:38:51 +0530

    updates

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cascadia Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Cascadia Mono:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true", 
+	"Symbols Nerd Font:pixelsize=14:antialias=true:autohint=true", 
 };
 
 static int borderpx = 8;
