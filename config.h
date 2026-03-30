2d50425f1e1a85549a0920f047d967bde71b9fa8
Rishi <rishibhargawa47@gmail.com>
2026-03-31 02:24:59 +0530

    modified

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Monospace:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = {
-  "Symbols Nerd Font:pixelsize=12:antialias=true:autohint=true",
+  "Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true",
 };
 static int borderpx = 8;
 
@@ -109,48 +109,52 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85;
+float alpha = 0.95;
 
-/* Habamax terminal colors (16 first used in escape sequence) */
+/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"#1c1c1c",
-	"#af5f5f",
-	"#5faf5f",
-	"#af875f",
-	"#5f87af",
-	"#af87af",
-	"#5f8787",
-	"#9e9e9e",
 
-	/* 8 bright colors */
-	"#767676",
-	"#d75f87",
-	"#87d787",
-	"#d7af87",
-	"#5fafd7",
-	"#d787d7",
-	"#87afaf",
-	"#bcbcbc",
+  /* 8 normal colors */
+  [0] = "#282a2e", /* black   */
+  [1] = "#a54242", /* red     */
+  [2] = "#8c9440", /* green   */
+  [3] = "#de935f", /* yellow  */
+  [4] = "#5f819d", /* blue    */
+  [5] = "#85678f", /* magenta */
+  [6] = "#5e8d87", /* cyan    */
+  [7] = "#707880", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#373b41", /* black   */
+  [9]  = "#cc6666", /* red     */
+  [10] = "#b5bd68", /* green   */
+  [11] = "#f0c674", /* yellow  */
+  [12] = "#81a2be", /* blue    */
+  [13] = "#b294bb", /* magenta */
+  [14] = "#8abeb7", /* cyan    */
+  [15] = "#c5c8c6", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#ffffff", /* cursor background */
-	"#000000", /* cursor foreground */
-	"#bcbcbc", /* foreground colour */
-	"#1c1c1c", /* background colour */
+  /* special colors */
+  [256] = "#1d1f21", /* background */
+  [257] = "#c5c8c6", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs=256;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
