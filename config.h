8367250bcd8e01535fb3bfaca3beb0d5673a49b7
Rishi <rishibhargawa47@gmail.com>
2026-04-06 18:52:35 +0530

    updates

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Monospace:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = {
-  "Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true",
+  "Symbols Nerd Font:pixelsize=14:antialias=true:autohint=true",
 };
 static int borderpx = 8;
 
@@ -109,34 +109,34 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.95;
+float alpha = 0.7;
+
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0] = "#282a2e", /* black   */
-  [1] = "#a54242", /* red     */
-  [2] = "#8c9440", /* green   */
-  [3] = "#de935f", /* yellow  */
-  [4] = "#5f819d", /* blue    */
-  [5] = "#85678f", /* magenta */
-  [6] = "#5e8d87", /* cyan    */
-  [7] = "#707880", /* white   */
+ /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#e92f2f", /* red     */
+  [2] = "#0ed839", /* green   */
+  [3] = "#dddd13", /* yellow  */
+  [4] = "#3b48e3", /* blue    */
+  [5] = "#f996e2", /* magenta */
+  [6] = "#23edda", /* cyan    */
+  [7] = "#ababab", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#373b41", /* black   */
-  [9]  = "#cc6666", /* red     */
-  [10] = "#b5bd68", /* green   */
-  [11] = "#f0c674", /* yellow  */
-  [12] = "#81a2be", /* blue    */
-  [13] = "#b294bb", /* magenta */
-  [14] = "#8abeb7", /* cyan    */
-  [15] = "#c5c8c6", /* white   */
+  [8]  = "#343434", /* black   */
+  [9]  = "#e92f2f", /* red     */
+  [10] = "#0ed839", /* green   */
+  [11] = "#dddd13", /* yellow  */
+  [12] = "#3b48e3", /* blue    */
+  [13] = "#f996e2", /* magenta */
+  [14] = "#23edda", /* cyan    */
+  [15] = "#f9f9f9", /* white   */
 
   /* special colors */
-  [256] = "#1d1f21", /* background */
-  [257] = "#c5c8c6", /* foreground */
+  [256] = "#000000", /* background */
+  [257] = "#ababab", /* foreground */
 };
 
 /*
@@ -146,7 +146,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 257;
-static unsigned int defaultrcs=256;
+static unsigned int defaultrcs = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
